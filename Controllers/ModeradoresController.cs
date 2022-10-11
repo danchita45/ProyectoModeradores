ca70ee9b6dacb25eedd29de73f5d8620992bb67c
Danchita45 <danchita45@gmail.com>
2022-10-11 15:02:47 -0500

    pagina de moderadores lista

---
+++ Controllers/ModeradoresController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoModeradores.Controllers
+{
+    public class ModeradoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       [HttpPost]
+        public IActionResult Consultar(string User, string Pass)
+        {
+            return Redirect(nameof(Index));
+        }
+    }
+}
